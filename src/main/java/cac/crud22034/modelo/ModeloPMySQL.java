4c0fd7b69b1bd2110e5cfbc7071d26b004ece375
Peeterlopez <pedrolopezcabral@gmail.com>
2022-07-16 17:45:41 -0300

    V.6 CRUD COMPLETO CON BARRA DE BUSQUEDA DOBLE

--- src/main/java/cac/crud22034/modelo/ModeloPMySQL.java
+++ src/main/java/cac/crud22034/modelo/ModeloPMySQL.java
@@ -23,9 +23,10 @@ public class ModeloPMySQL implements ModeloP {
     
     private static final String GET_ALL_QUERY="SELECT * FROM productos";
     private static final String GET_BY_ID_QUERY = "SELECT * FROM productos WHERE id = ?";
-    private static final String ADD_QUERY = "INSERT INTO productos VALUES (null, ?, ?, ?, ?, ?)";
-    private static final String UPDATE_QUERY = "UPDATE productos SET nombre=?, apellido=?, fechaNacimiento = ?, mail=?, fotoBase64=? WHERE id=?";
+    private static final String ADD_QUERY = "INSERT INTO productos VALUES (null, ?, ?, ?, ?)";
+    private static final String UPDATE_QUERY = "UPDATE productos SET nombre=?, precio=?, cantidad = ?, fotoBase64=? WHERE id=?";
     private static final String DELETE_QUERY = "DELETE FROM productos WHERE id = ?";
+    private static final String GET_BY_NAME_QUERY="SELECT * FROM productos WHERE nombre = ?";
     
 
     @Override
@@ -68,22 +69,63 @@ public class ModeloPMySQL implements ModeloP {
 
     @Override
     public int addProducto(Producto producto) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int regsAgregados = 0;
+    try ( Connection con = Conexion.getConnection();  PreparedStatement ps = con.prepareStatement(ADD_QUERY);) {
+            fillPreparedStatement(ps, producto);
+            regsAgregados = ps.executeUpdate();
+        } catch (SQLException ex) {
+            throw new RuntimeException("Error de SQL", ex);
+        } catch (Exception ex) {
+            throw new RuntimeException("Error al agregar producto " + producto.getNombre(), ex);
+        }
+        return regsAgregados;
     }
 
     @Override
     public int updateProducto(Producto producto) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int regsActualizados = 0;
+        try ( Connection con = Conexion.getConnection();  PreparedStatement ps = con.prepareStatement(UPDATE_QUERY);) {
+            fillPreparedStatement(ps, producto);
+            ps.setInt(5, producto.getId());
+            regsActualizados = ps.executeUpdate();
+        } catch (SQLException ex) {
+            throw new RuntimeException("Error de SQL", ex);
+        } catch (Exception ex) {
+            throw new RuntimeException("Error al editar alumno " + producto.getNombre(), ex);
+        }
+        return regsActualizados;
     }
 
     @Override
     public int removeProducto(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+       int regsBorrados = 0;
+        try ( Connection con = Conexion.getConnection();  PreparedStatement ps = con.prepareStatement(DELETE_QUERY);) {
+            ps.setInt(1, id);
+            regsBorrados = ps.executeUpdate();
+        } catch (SQLException ex) {
+            throw new RuntimeException("Error de SQL", ex);
+        } catch (Exception ex) {
+            throw new RuntimeException("Error al borrar alumno con ID " + id, ex);
+        }
+        return regsBorrados;
     }
 
     @Override
     public Producto buscarProducto(String nombre) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+       Producto p = null;
+        try (Connection con = Conexion.getConnection(); PreparedStatement ps = con.prepareStatement(GET_BY_NAME_QUERY); ){
+        ps.setString(1, nombre);
+        try(ResultSet rs =ps.executeQuery();){
+            rs.next();
+            p=rsToProducto(rs);
+        }
+                
+        } catch (SQLException ex) {
+            throw new RuntimeException("Error de SQL", ex);
+        } catch (Exception ex){
+            throw new RuntimeException("Error al leer alumno con ID " +nombre,ex);
+        }
+        return p;
     }
 
     private Producto rsToProducto (ResultSet rs) throws SQLException {
@@ -95,6 +137,13 @@ public class ModeloPMySQL implements ModeloP {
                 Producto p = new Producto(idP, nombre, precio, cantidad, foto);
         return new Producto(idP, nombre, precio, cantidad, foto);
     }
+    
+    private void fillPreparedStatement(PreparedStatement ps, Producto producto) throws SQLException {
+        ps.setString(1, producto.getNombre());
+        ps.setFloat(2, producto.getPrecio());
+        ps.setInt(3, producto.getCantidad());
+        ps.setString(4, producto.getFoto());
+        }
     
     
 }
